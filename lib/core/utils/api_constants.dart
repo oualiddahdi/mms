53b7e5a02f2e126e08fb6dc0b4dec947cc916d5a
oualiddahdi <57314557+oualiddahdi@users.noreply.github.com>
2024-07-23 15:47:32 +0300

    edit toast Login

--- lib/core/utils/api_constants.dart
+++ lib/core/utils/api_constants.dart
@@ -3,21 +3,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiConstants extends GetConnect {
   static String _baseUrl = 'http://192.168.100.250:8080/manageProjects/api/';
-  static const String _apiUrlDoc = 'http://192.168.100.250:8080/manageProjects/';
+  static String _baseUrlDoc = 'http://192.168.100.250:8080/manageProjects/';
 
   static Future<void> initialize() async {
     final prefs = await SharedPreferences.getInstance();
     _baseUrl = prefs.getString('apiUrl') ?? _baseUrl;
+    _baseUrlDoc = prefs.getString('apiUrlDoc') ?? _baseUrlDoc;
   }
 
-  static Future<void> setApiUrl(String newUrl) async {
+  static Future<void> setBaseUrl(String newBaseUrl) async {
     final prefs = await SharedPreferences.getInstance();
-    _baseUrl = newUrl;
-    await prefs.setString('apiUrl', newUrl);
+    _baseUrl = newBaseUrl;
+    _baseUrlDoc = newBaseUrl.replaceAll('/api/', '/'); // Adjust if necessary
+
+    await prefs.setString('apiUrl', _baseUrl);
+    await prefs.setString('apiUrlDoc', _baseUrlDoc);
   }
 
   static String get apiUrl => _baseUrl;
-  static String get apiUrlDoc => _apiUrlDoc;
+  static String get apiUrlDoc => _baseUrlDoc;
 
   static String get loginUrl => '${apiUrl}login';
   static String get otpUrl => '${apiUrl}otp';
